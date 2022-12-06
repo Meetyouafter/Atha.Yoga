608e7d1141df246fcfbea3a94fc5a2855961093d
Levis <anton_levus@mail.ru>
2022-12-06 17:19:33 +0600

    updated validation forms

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/jsx-props-no-spreading */
 import React from 'react';
 import {
   TextField,
@@ -5,33 +6,22 @@ import {
   Typography,
   Container,
   FormGroup,
+  Grid,
 } from '@mui/material';
 import { useForm } from 'react-hook-form';
-import { yupResolver } from '@hookform/resolvers/yup';
-import * as Yup from 'yup';
 import { useNavigate } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import { getUserData } from '../../store/userSlice';
 
 const Login = () => {
   const dispatch = useDispatch();
-
   const navigate = useNavigate();
-
-  const validationSchema = Yup.object().shape({
-    email: Yup.string()
-      .required('Поле обязательно для заполнения'),
-    password: Yup.string()
-      .required('Поле обязательно для заполнения'),
-  });
 
   const {
     register,
     handleSubmit,
     formState: { errors },
-  } = useForm({
-    resolver: yupResolver(validationSchema),
-  });
+  } = useForm();
 
   const onSubmit = data => {
     fetch('http://localhost:3000/login', {
@@ -43,6 +33,7 @@ const Login = () => {
       .then(responseData => {
         dispatch(getUserData(responseData));
         if (responseData.user === undefined) {
+          // eslint-disable-next-line no-alert
           return alert('Введены неверные данные');
         }
         localStorage.setItem('accessToken', responseData.accessToken);
@@ -51,47 +42,75 @@ const Login = () => {
   };
 
   return (
-    <Container maxWidth="sm">
-      <Typography variant="h2" sx={{ textAlign: 'center', paddingBottom: '30px' }}>Войти</Typography>
-      <FormGroup
-        sx={{
-          display: 'flex',
-          alignItems: 'center',
-          flexDirection: 'column',
-          minHeight: '100vh',
-        }}
-        onSubmit={handleSubmit(onSubmit)}
-      >
-        <TextField
-          id="email"
-          label="E-mail"
-          size="normal"
-          margin="normal"
-          fullWidth
-        />
-        <TextField
-          id="password"
-          label="Пароль"
-          type="password"
-          size="normal"
-          margin="normal"
-          fullWidth
-        />
-        <Button
-          variant="contained"
-          type="submit"
-          size="large"
-          sx={{
-            marginTop: '50px',
-            marginBottom: '20px',
-            padding: '15px',
-            width: '70%',
-          }}
-        >
-          Войти
-        </Button>
-      </FormGroup>
-    </Container>
+    <Grid
+      container
+      alignItems="center"
+      justifyContent="center"
+      direction="column"
+      sx={{ minHeight: '100vh' }}
+    >
+      <Container maxWidth="sm">
+        <Typography variant="h2" sx={{ textAlign: 'center', paddingBottom: '30px' }}>Войти</Typography>
+
+        <form style={{ height: '300px' }} onSubmit={handleSubmit(onSubmit)}>
+          <FormGroup
+            sx={{
+              display: 'flex',
+              alignItems: 'center',
+              flexDirection: 'column',
+              minHeight: '100vh',
+            }}
+          >
+            <TextField
+              id="email"
+              label="E-mail"
+              size="normal"
+              margin="normal"
+              fullWidth
+              {...register('email', {
+                required: 'Поле обязательно для заполнения',
+                pattern: {
+                  value: /^\S+@\S+\.\S+$/,
+                  message: 'Invalid',
+                },
+              })}
+              error={!!errors?.email}
+              helperText={errors?.email ? errors.email.message : ''}
+            />
+            <TextField
+              id="password"
+              label="Пароль"
+              type="password"
+              size="normal"
+              margin="normal"
+              fullWidth
+              {...register('password', {
+                required: 'Поле обязательно для заполнения',
+                pattern: {
+                  value: /.{10,}/,
+                  message: 'Пароль должен содержать не менее 10 символов',
+                },
+              })}
+              error={!!errors?.password}
+              helperText={errors?.password ? errors.password.message : ''}
+            />
+            <Button
+              variant="contained"
+              type="submit"
+              size="large"
+              sx={{
+                marginTop: '50px',
+                marginBottom: '20px',
+                padding: '15px',
+                width: '70%',
+              }}
+            >
+              Войти
+            </Button>
+          </FormGroup>
+        </form>
+      </Container>
+    </Grid>
   );
 };
 
--- src/components/SignUp/SignUp.jsx
+++ src/components/SignUp/SignUp.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/jsx-props-no-spreading */
 import React, { useState } from 'react';
 import {
   TextField,
@@ -8,37 +9,21 @@ import {
   Container,
   Stack,
   FormGroup,
+  Grid,
 } from '@mui/material';
 import { Link } from 'react-router-dom';
 import { useForm } from 'react-hook-form';
-import { yupResolver } from '@hookform/resolvers/yup';
-import * as Yup from 'yup';
 import MailSend from './MailSend';
+import './SignUp.scoped.scss';
 
 const SignUp = () => {
-  const [isSuccess, setIsSuccess] = useState(true);
-
-  const validationSchema = Yup.object().shape({
-    name: Yup.string().required('Поле обязательно для заполнения'),
-    email: Yup.string()
-      .required('Поле обязательно для заполнения')
-      .email('Пожалуйста, введите действительный адрес электронной почты (например, name@yandex.ru)'),
-    password: Yup.string()
-      .required('Поле обязательно для заполнения')
-      .min(10, 'Пароль должен быть не меньше 10 символов'),
-    acceptTerms: Yup.bool().oneOf(
-      [true],
-      'Необходимо принять правила пользовательского соглашения',
-    ),
-  });
+  const [isSuccess, setIsSuccess] = useState(false);
 
   const {
     register,
     handleSubmit,
     formState: { errors },
-  } = useForm({
-    resolver: yupResolver(validationSchema),
-  });
+  } = useForm();
 
   const onSubmit = data => {
     //  console.log(JSON.stringify(data, null, 2));
@@ -53,58 +38,94 @@ const SignUp = () => {
 
   if (!isSuccess) {
     return (
-      <Container maxWidth="sm">
-        <Typography variant="h2" sx={{ textAlign: 'center', paddingBottom: '30px' }}>Создать аккаунт</Typography>
-        <FormGroup
-          display="flex"
-          justifyContent="center"
-          alignItems="center"
-          flexDirection="column"
-          minHeight="100vh"
-          sx={{
-            border: '1px solid #E0E0E0',
-            borderRadius: '4px',
-            padding: '20px',
-          }}
-        >
-          <TextField
-            id="name"
-            label="Имя"
-            size="normal"
-            margin="normal"
-            fullWidth
-          />
-          <TextField
-            id="email"
-            label="E-mail"
-            size="normal"
-            margin="normal"
-            fullWidth
-          />
-          <TextField
-            id="password"
-            label="Пароль"
-            type="password"
-            size="normal"
-            margin="normal"
-            fullWidth
-          />
-          <FormControlLabel control={<Checkbox />} label="Я согласен получать рассылку" />
-          <FormControlLabel control={<Checkbox />} label="Я принимаю условия пользовательского соглашения" />
-          <Button
-            variant="contained"
-            type="submit"
-            size="large"
-            sx={{ marginTop: '50px', marginBottom: '20px', padding: '15px' }}
-          >
-            Создать
-          </Button>
-          <Stack direction="row" spacing={2} sx={{ justifyContent: 'center' }}>
-            <Typography varinat="subtitle1">Уже есть аккаунт?</Typography>
-            <Typography varinat="subtitle2" component={Link} to="/login">Войти</Typography>
-          </Stack>
-        </FormGroup>
-      </Container>
+      <Grid
+        container
+        alignItems="center"
+        justifyContent="center"
+        direction="column"
+        sx={{ minHeight: '100vh' }}
+      >
+        <Container maxWidth="sm">
+          <Typography variant="h2" sx={{ textAlign: 'center', paddingBottom: '30px' }}>Создать аккаунт</Typography>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <FormGroup
+              sx={{
+                border: '1px solid #E0E0E0',
+                borderRadius: '4px',
+                padding: '20px',
+              }}
+            >
+              <TextField
+                id="name"
+                label="Имя"
+                size="normal"
+                margin="normal"
+                fullWidth
+                {...register('name', { required: 'Поле обязательно для заполнения' })}
+                error={!!errors?.name}
+                helperText={errors?.name ? errors.name.message : ''}
+              />
+              <TextField
+                id="email"
+                label="E-mail"
+                size="normal"
+                margin="normal"
+                fullWidth
+                {...register('email', {
+                  required: 'Поле обязательно для заполнения',
+                  pattern: {
+                    value: /^\S+@\S+\.\S+$/,
+                    message: 'Invalid',
+                  },
+                })}
+                error={!!errors?.email}
+                helperText={errors?.email ? errors.email.message : ''}
+              />
+              <TextField
+                id="password"
+                label="Пароль"
+                type="password"
+                size="normal"
+                margin="normal"
+                fullWidth
+                {...register('password', {
+                  required: 'Поле обязательно для заполнения',
+                  pattern: {
+                    value: /.{10,}/,
+                    message: 'Пароль должен содержать не менее 10 символов',
+                  },
+                })}
+                error={!!errors?.password}
+                helperText={errors?.password ? errors.password.message : ''}
+              />
+              <FormControlLabel
+                control={<Checkbox />}
+                label="Я согласен получать рассылку"
+                {...register('mailDelivery')}
+              />
+              <FormControlLabel
+                control={<Checkbox />}
+                label="Я принимаю условия пользовательского соглашения"
+                {...register('acceptTerms', { required: 'Необходимо принять правила пользовательского соглашения' })}
+                error={!!errors?.acceptTerms}
+              />
+              <div className="error">{errors?.acceptTerms ? errors.acceptTerms.message : ''}</div>
+              <Button
+                variant="contained"
+                type="submit"
+                size="large"
+                sx={{ marginTop: '50px', marginBottom: '20px', padding: '15px' }}
+              >
+                Создать
+              </Button>
+              <Stack direction="row" spacing={2} sx={{ justifyContent: 'center' }}>
+                <Typography varinat="subtitle1">Уже есть аккаунт?</Typography>
+                <Typography varinat="subtitle2" component={Link} to="/login">Войти</Typography>
+              </Stack>
+            </FormGroup>
+          </form>
+        </Container>
+      </Grid>
     );
   }
   return <MailSend />;
